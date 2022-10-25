4a84dc46f3e45c4c6f6f84968288e6a8d6ce05a2
paul1610 <57170160+paul1610@users.noreply.github.com>
2022-10-25 13:33:43 +0200

    Update Program.cs

--- 01_GeneticDrift/GeneticDrift/GeneticDrift/Program.cs
+++ 01_GeneticDrift/GeneticDrift/GeneticDrift/Program.cs
@@ -11,42 +11,20 @@ namespace GeneticDrift
             Output(inputInt);
         }
 
-        static int[] CombineArrays(int[] x, int[] yourNumber)
+        static int[] PushToArray(int[] x, int yourNumber)
         {
-            int[] y = new int[x.Length + yourNumber.Length];
+            int[] y = new int[x.Length + 1];
             for (int i = 0; i < x.Length; i++)
             {
                 y[i] = x[i];
             }
-            for (int i = x.Length; i < yourNumber.Length + x.Length; i++)
-            {
-                y[i] = yourNumber[i - x.Length - 1];
-            }
+            y[x.Length] = yourNumber;
             return y;
-        }
-
-        static int[] FindOrientedPaars(int[] inputList)
-        {
-            int[] outputList = new int[0];
-            int[] temp = new int[2];
-            for (int i = 0; i < inputList.Length; i++)
-            {
-                for (int n = i; n < inputList.Length; n++)
-                {
-                    if (inputList[n] + inputList[i] == -1 || inputList[n] + inputList[i] == 1)
-                    {
-                        temp[0] = inputList[n];
-                        temp[1] = inputList[i];
-                        outputList = CombineArrays(outputList, temp);
-                    }
-                }
-            }
-            return outputList;
         }
 
         static void Output(int[] output)
         {
-            Console.WriteLine($"Output: {String.Join(", ", output)}");
+            Console.WriteLine($"Output: {String.Join(" ", output)}");
         }
-    }
+    }x
 }
