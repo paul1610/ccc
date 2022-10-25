52788bc2a0543cc367365a7af979e52eab08fc27
Jonas Pamminger <jonas.pamminger@outlook.de>
2022-10-25 12:59:19 +0200

    fibonacci solution

--- 00_Fibonacci/Fibonacci.ConsoleApp1/Fibonacci.ConsoleApp1/Program.cs
+++ 00_Fibonacci/Fibonacci.ConsoleApp1/Fibonacci.ConsoleApp1/Program.cs
@@ -6,7 +6,16 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int a = 0, b = 1, c = 0;
+            for (int i = 2; i < 100; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine($"{i}: {b}");
+            }
+            
         }
     }
 }
