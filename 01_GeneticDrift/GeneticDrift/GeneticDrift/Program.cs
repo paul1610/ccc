4029956aa7ac67a7055b5ffae0457ead62584d19
Jonas-Pamminger <85544687+Jonas-Pamminger@users.noreply.github.com>
2022-10-25 15:53:19 +0200

    Update Program.cs

--- 01_GeneticDrift/GeneticDrift/GeneticDrift/Program.cs
+++ 01_GeneticDrift/GeneticDrift/GeneticDrift/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Dynamic;
 
 namespace GeneticDrift
 {
@@ -9,8 +10,33 @@ namespace GeneticDrift
             string input = "8 0 3 1 6 5 -2 4 7";
             int[] inputInt = Array.ConvertAll(input.Split(' '), int.Parse);
             inputInt = FindOrientedPaars(inputInt);
-            inputInt = Sort(inputInt);
-            Output(inputInt);
+            inputInt = LolDieAngabeIsDumm(inputInt);
+            foreach (int item in inputInt)
+            {
+                Console.Write(item);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+            List<Pair> pairs = GetPairs(inputInt);
+            Output(pairs);
+            Console.WriteLine(  );
+            List<Pair> sortedList = Sort(pairs);
+            Output(sortedList);
+        }
+
+        static int[] LolDieAngabeIsDumm(int[] x)
+        {
+            int temp;
+            for(int i = 0; i < x.Length; i += 2)
+            {
+                if (x[i] < x[i + 1])
+                {
+                    temp = x[i + 1];
+                    x[i + 1] = x[i];
+                    x[i + 1] = temp;
+                }
+            }
+            return x;
         }
 
         static int[] CombineArrays(int[] x, int[] yourNumber)
@@ -27,21 +53,20 @@ namespace GeneticDrift
             return y;
         }
 
-        static int[] Sort(int[] arr)
+        static List<Pair> Sort(List<Pair> arr)
         {
-            int temp, temp1;
-            for (int i = 0; i < arr.Length; i += 2)
-            {
-                for (int j = 0; j < arr.Length; j += 2)
+            int temp, changes = 1;
+
+            while(changes != 0) { 
+                changes = 0;
+                for (int i = 1; i < arr.Count; i++)
                 {
-                    if (arr[j] >= arr[i])
+                    if (arr[i-1].X > arr[i].X)
                     {
-                        temp = arr[j];
-                        temp1 = arr[j + 1];
-                        arr[j] = arr[i + 1];
-                        arr[j + 1] = arr[i];
-                        arr[i] = temp1;
-                        arr[i + 1] = temp;
+                        changes++;
+                        Pair tmp = arr[i-1];
+                        arr[i-1] = arr[i];
+                        arr[i] = tmp;
                     }
                 }
             }
@@ -54,7 +79,7 @@ namespace GeneticDrift
             int[] temp = new int[2];
             for (int i = 0; i < inputList.Length; i++)
             {
-                for (int n = 0; n < inputList.Length; n++)
+                for (int n = i; n < inputList.Length; n++)
                 {
                     if (inputList[n] + inputList[i] == -1 || inputList[n] + inputList[i] == 1)
                     {
@@ -66,10 +91,43 @@ namespace GeneticDrift
             }
             return outputList;
         }
-
-        static void Output(int[] output)
+        
+        public static List<Pair> GetPairs(int[] input)
         {
-            Console.WriteLine($"Output: {output.Length / 2} {String.Join(" ", output)}");
+            List<Pair> pairs = new List<Pair>();
+            Pair tmp = new Pair();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(tmp.X == null)
+                {
+                    tmp.X = input[i];
+                }
+                else
+                {
+                    tmp.Y = input[i];
+                    if(tmp.X < tmp.Y)
+                    {
+                        int? inte = tmp.Y;
+                        tmp.Y = tmp.X;
+                        tmp.X = inte;
+                    }
+                    if (tmp.X != 0 && tmp.Y != 0)
+                    {
+                        pairs.Add(tmp);
+                    }
+                    tmp = new Pair();
+                }
+            }
+            return pairs;
+        }
+
+        static void Output(List<Pair> output)
+        {
+            Console.Write(output.Count);
+            foreach(var item in output)
+            {
+                Console.Write($" {item.X} {item.Y}");
+            }
         }
     }
 }
