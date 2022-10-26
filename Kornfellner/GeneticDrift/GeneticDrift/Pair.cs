8d5adac8b239f32b1da77a15c76dfd97f2f94dc1
Alexander Kornfellner <a.kornfellner@htl-leonding.ac.at>
2022-10-26 14:57:21 +0200

    level 1 finished

---
+++ Kornfellner/GeneticDrift/GeneticDrift/Pair.cs
@@ -0,0 +1,33 @@
+﻿namespace Test
+{
+    public class Pair : IComparable<Pair>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Pair(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{X} {Y}";
+        }
+
+        public int CompareTo(Pair? other)
+        {
+            if (other == null)
+            {
+                return 0;
+            }
+
+            if (X < other.X)
+                return -1;
+            if (X == other.X)
+                return 0;
+            return 1;
+        }
+    }
+}
