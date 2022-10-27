98a85fff23960a4738db72efb6ed4f12dfcd8437
Jonas Pamminger <jonas.pamminger@outlook.de>
2022-10-27 07:05:38 +0200

    change solution for genetic drift lvl 1

---
+++ 01_GeneticDrift/GeneticDrift/Permutation.cs
@@ -0,0 +1,74 @@
+namespace Test;
+
+public class Permutation
+{
+    public int Length { get; }
+    public int[] Numbers { get; }
+
+    public Permutation(string input)
+    {
+        string[] parts = input.Split(' ');
+
+        Length = Convert.ToInt32(parts[0]);
+
+        int[] numbers = new int[parts.Length - 1];
+
+        for (int i = 1; i < parts.Length; i++)
+        {
+            numbers[i - 1] = Convert.ToInt32(parts[i]);
+        }
+
+        Numbers = numbers;
+    }
+
+    public string Level1()
+    {
+        int count = 0;
+
+        List<Pair> pairs = new List<Pair>();
+
+        for (int i = 0; i < Numbers.Length; i++)
+        {
+            for (int j = i + 1; j < Numbers.Length; j++)
+            {
+                if (Numbers[i] == 0 && Numbers[j] > 0 || Numbers[i] > 0 && Numbers[j] == 0)
+                {
+                    continue;
+                }
+
+                int value = Numbers[i] * -1 - Numbers[j];
+                value = Math.Abs(value);
+
+                if (value == 1)
+                {
+                    count++;
+                    Pair pair = new Pair(Numbers[i], Numbers[j]);
+                    pairs.Add(pair);
+                }
+            }
+        }
+
+        pairs.Sort();
+
+        string output = count.ToString();
+
+        foreach (var pair in pairs)
+        {
+            output += $" {pair}";
+        }
+
+        return output;
+    }
+
+    public override string ToString()
+    {
+        string output = Length.ToString();
+
+        foreach (var number in Numbers)
+        {
+            output += $" {number.ToString()}";
+        }
+
+        return output;
+    }
+}
---
+++ 01_GeneticDrift/GeneticDrift/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "6 3 1 6 5 -2 4";
+            Permutation test = new Permutation(input);
+            Console.WriteLine(test.Level1());
+        }
+    }
+}
