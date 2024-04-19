6b1285a6e74ef853b41628465143e60d6b832df2
Paul Nell <57170160+paul1610@users.noreply.github.com>
2024-04-19 07:46:04 +0200

    Add util

--- 19.04.2024/ConsoleApp1/ConsoleApp1/Util/FileUtil.cs
+++ 19.04.2024/ConsoleApp1/ConsoleApp1/Util/FileUtil.cs
@@ -8,5 +8,29 @@ namespace ConsoleApp1.Util
 {
     internal class FileUtil
     {
+        public static string[] ReadLines(string filePath)
+        {
+            return File.ReadAllLines(filePath);
+        }
+
+        public static string ReadText(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        public static List<string[]> ParseCSV(string filePath)
+        {
+            List<string[]> records = new List<string[]>();
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(',');
+                    records.Add(values);
+                }
+            }
+            return records;
+        }
     }
 }
--- 19.04.2024/ConsoleApp1/ConsoleApp1/Util/MiscUtil.cs
+++ 19.04.2024/ConsoleApp1/ConsoleApp1/Util/MiscUtil.cs
@@ -8,5 +8,38 @@ namespace ConsoleApp1.Util
 {
     internal class MiscUtil
     {
+        public static T[] CloneArray<T>(T[] array)
+        {
+            return (T[])array.Clone();
+        }
+
+        public static T[] MergeArrays<T>(params T[][] arrays)
+        {
+            return arrays.SelectMany(x => x).ToArray();
+        }
+
+        public static DateTime AddBusinessDays(DateTime date, int days)
+        {
+            int sign = Math.Sign(days);
+            while (days != 0)
+            {
+                date = date.AddDays(sign);
+                if (date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday)
+                    days -= sign;
+            }
+            return date;
+        }
+
+        private static Random _random = new Random();
+
+        public static int GenerateRandomNumber(int min, int max)
+        {
+            return _random.Next(min, max + 1);
+        }
+
+        public static T GetRandomElement<T>(List<T> list)
+        {
+            return list[GenerateRandomNumber(0, list.Count - 1)];
+        }
     }
 }
